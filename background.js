2d1dade1600313daa3fe86d5e5740fc3989704da
Vladislav Penchuk <Vlad@TheResearchCorp.com>
2023-04-14 01:19:35 -0700

    fix keyboard shortcut not working

--- background.js
+++ background.js
@@ -1,5 +1,15 @@
+chrome.commands.onCommand.addListener((command) => {
+  if (command === "start_listening") {
+    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+      chrome.tabs.executeScript(tabs[0].id, { file: "content.js" }, () => {
+        chrome.tabs.sendMessage(tabs[0].id, { action: "startSpeechRecognition" });
+      });
+    });
+  }
+});
+
 chrome.browserAction.onClicked.addListener((tab) => {
-  chrome.tabs.executeScript(tab.id, {
-    file: "content.js",
+  chrome.tabs.executeScript(tab.id, { file: "content.js" }, () => {
+    chrome.tabs.sendMessage(tab.id, { action: "startSpeechRecognition" });
   });
 });
